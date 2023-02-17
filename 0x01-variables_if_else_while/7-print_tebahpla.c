5c468bcdb4ec341f5812d8def6a90ee5c60e1f04
fafi-make <tebibu374@gmail.com>
2023-02-17 03:59:04 -0800

    C var&loop

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,22 +2,14 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char c;
+	char c;
 
-c = 'a';
-while (c <= 'z')
-{
-if (c != 'q' && c != 'e')
-{
-putchar(c);
-}
-c++;
-}
-putchar(10);
-return (0);
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
 }
